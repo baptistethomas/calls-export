db77c0b6d0cf60de0f2aee514c697f51c8e2169d
Baptiste Thomas <thomas.bapt@gmail.com>
2024-06-05 12:54:13 +0200

    build : hot reload for nestjs / add typeorm & config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { configService } from './config/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
